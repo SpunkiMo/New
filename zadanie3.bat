9cf8c4bd3bba1c5e01818001dc718ff062eaaf9f
Danil <kovtun2517@gmail.com>
2022-02-08 19:13:40 +0400

    New commit

--- zadanie3.bat
+++ zadanie3.bat
@@ -5,6 +5,6 @@ chcp 65001
 For /F "Skip=1" %%i In ('WMIC Path Win32_LocalTime Get DayOfWeek') do Set /A DayOfWeek=%%i &GoTo Da 
 :Da
 
-if %DayOfWeek%==4 (set /p "hi=Нужен дополнительный акцепт, продолжить установку? Y/N: ") else ( exit /b )
+if %DayOfWeek% == 2 ( set /p "hi=Нужен дополнительный акцепт, продолжить установку? Y/N: ") else ( exit ) 
 
-if /i "%hi%"=="y" ( echo "Окей" ) else (if "%hi%"=="n" ( exit /b ) else echo "Меняй букву." &goto Da)
+if /i "%hi%" == "y" ( echo "Окей, повторим!" &goto Da) else (if "%hi%"=="n" ( exit ) else echo "Меняй букву." &goto Da)
