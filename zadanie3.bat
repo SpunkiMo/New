c886509053867e3c82f0746eb14cf7aa8696bee9
Ковтуненко Даниил Дмитриевич <kovtun@RARUS-SAMARA.LOCAL>
2022-02-09 11:29:27 +0400

    Bez kodirovki

--- zadanie3.bat
+++ zadanie3.bat
@@ -1,10 +1,8 @@
 @Echo Off
 
-chcp 65001
-
 For /F "Skip=1" %%i In ('WMIC Path Win32_LocalTime Get DayOfWeek') do Set /A DayOfWeek=%%i &GoTo Da 
 :Da
 
-if %DayOfWeek% == 2 ( set /p "hi=Нужен дополнительный акцепт, продолжить установку? Y/N: ") else ( exit ) 
+if %DayOfWeek% == 3 ( set /p "hi=Nuzhen dopolnitelnii akcept, prodolzhit ustanovku? Y/N: ") else ( exit ) 
 
-if /i "%hi%" == "y" ( echo "Окей, повторим!" &goto Da) else (if "%hi%"=="n" ( exit ) else echo "Меняй букву." &goto Da)
+if /i "%hi%" == "y" ( echo "Ok, povtorim!" &goto Da) else (if /i "%hi%"=="n" ( exit ) else echo "Meniai bukvu!." &goto Da)
